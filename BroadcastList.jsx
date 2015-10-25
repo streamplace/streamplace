64cd068b41ae06ef74d6377e7a6850a98dda9101
Eli Mallon <eli.mallon@csats.com>
2015-10-25 11:34:56 +0000

    Getting the hang of this.

---
+++ BroadcastList.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+export default React.createClass({
+  displayName: 'BroadcastList',
+  render: function(){
+    return (
+      <div>
+        <h3>Current Broadcasts</h3>
+
+      </div>
+    )
+  }
+})
--- Gort.jsx
+++ Gort.jsx
@@ -1,11 +1,17 @@
 
 import React from "react";
 
-require("./gort.scss")
+import {} from "./gort.scss"
+import BroadcastList from "./BroadcastList";
 
 export default React.createClass({
-    displayName: 'Gort',
-    render: function(){
-        return <div>Hello React</div>
-    }
+  displayName: 'Gort',
+  render: function() {
+    return (
+      <div>
+        <h1>streamprov studio</h1>
+        <BroadcastList />
+      </div>
+    )
+  }
 })

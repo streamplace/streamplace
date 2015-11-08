1265412cbebe4934ddda712b25664219f1fff877
Eli Mallon <eli.mallon@csats.com>
2015-11-08 03:32:30 +0000

    add new mote panel exists

--- components/motes/MoteCreate.jsx
+++ components/motes/MoteCreate.jsx
@@ -1,13 +1,13 @@
 
 import React from "react";
 import { Mote } from "bellamie";
-import style from "./MoteDetail.scss";
+import style from "./MoteCreate.scss";
 
 export default React.createClass({
   render() {
     return (
       <section>
-        <span className={style.noMoteSelected}> - No Mote Selected - </span>
+        Create a mote!
       </section>
     )
   }

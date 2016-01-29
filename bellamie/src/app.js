153c88064e54074c1a441c5d8768c19f9267cb54
Eli Mallon <eli.mallon@csats.com>
2016-01-29 10:56:14 +0000

    Dynamic loading of resources from Swagger

--- bellamie/src/app.js
+++ bellamie/src/app.js
@@ -1,10 +1,39 @@
 
 import express from "express";
 import winston from "winston";
-
+import schema from "skitchen-schema";
+import SwaggerParser from "swagger-parser";
 import ENV from "./env";
 
 const app = express();
+
+// Make winston output pretty
+winston.cli();
+
+///////////////////////////////////////////////////////////////////////////////
+// Step 1: parse the swagger schema and use that to build our routing table. //
+///////////////////////////////////////////////////////////////////////////////
+
+const resources = {};
+
+SwaggerParser.parse(schema)
+
+.then(function(parsed) {
+  const paths = Object.keys(parsed.paths);
+  paths.forEach(function(path) {
+    // Get the resource. It's the thing right after the slash.
+    const resourceName = path.split("/")[1];
+    if (!resources[resourceName]) {
+      winston.info(`Registering resource: ${resourceName}`);
+      resources[resourceName] = require(`./resources/${resourceName}`);
+    }
+  });
+})
+
+.catch(function(err) {
+  winston.error("Error parsing schema", err);
+  throw err;
+});
 
 if (!module.parent) {
   app.listen(ENV.PORT);

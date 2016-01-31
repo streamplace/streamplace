f2792f123f42eff946eb2fc60ba1d8cf8d9d2c75
Eli Mallon <eli.mallon@csats.com>
2016-01-31 15:22:52 +0000

    fixup

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 filesToLint="$(git diff --cached --name-only | grep "\.js$")"
-echo "$filesToLint"
-echo "test"
 if [[ -n "$filesToLint" ]]; then
   echo "$filesToLint" | xargs -L 1 eslint
 else

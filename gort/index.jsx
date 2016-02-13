4c158fa45b77cd4a62e0c3160ca7f059280dfc02
Eli Mallon <eli.mallon@csats.com>
2016-02-13 08:19:08 +0000

    Okay, Gort is broadly working, time to build more

--- gort/index.jsx
+++ gort/index.jsx
@@ -1,9 +1,10 @@
 
+// import createBrowserHistory from 'history/lib/createBrowserHistory'
 import ReactDOM from 'react-dom';
-import { Router, Route, Link } from 'react-router';
+import { Router, Route, Link, browserHistory } from 'react-router';
 
-import BroadcastIndex from './components/broadcasts/BroadcastIndex';
-import BroadcastDetail from './components/broadcasts/BroadcastDetail';
+// import BroadcastIndex from './components/broadcasts/BroadcastIndex';
+// import BroadcastDetail from './components/broadcasts/BroadcastDetail';
 import NotFound from './components/NotFound';
 
 import {} from "./components/main";
@@ -11,9 +12,9 @@ import {} from "font-awesome-webpack";
 import {} from "twixtykit/base.scss";
 
 ReactDOM.render((
-  <Router>
-    <Route path="/" component={BroadcastIndex} />
-    <Route path="/broadcasts/:broadcastId" component={BroadcastDetail} />
+  <Router history={browserHistory}>
+    { /* <Route path="/" component={BroadcastIndex} /> */ }
+    { /* <Route path="/broadcasts/:broadcastId" component={BroadcastDetail} /> */ }
     <Route path="*" component={NotFound} />
   </Router>
 ), document.querySelector('main'));

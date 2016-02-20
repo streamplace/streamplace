ea223499a5607873be528b9170931b0204b8c6e2
Eli Mallon <eli.mallon@csats.com>
2016-02-20 01:09:41 +0000

    Clean up Bellamie a bit

---
+++ bellamie/src/util.js
@@ -0,0 +1,39 @@
+
+import r from "rethinkdb";
+import winston from "winston";
+
+export function ensureTableExists(name, conn) {
+  return new Promise(function(resolve, reject) {
+    r.tableCreate(name).run(conn).then(function() {
+      winston.info(`Created table ${name}`);
+      resolve();
+    })
+    .catch(function(err) {
+      // Already exists, that's fine.
+      if (err.msg.indexOf("already exists") !== -1) {
+        resolve();
+      }
+      else {
+        reject(err);
+      }
+    });
+  });
+}
+
+export function ensureDatabaseExists(name, conn) {
+  return new Promise(function(resolve, reject) {
+    r.dbCreate(name).run(conn).then(function() {
+      winston.info(`Created database ${name}`);
+      resolve();
+    })
+    .catch(function(err) {
+      // Already exists, that's fine.
+      if (err.msg.indexOf("already exists") !== -1) {
+        resolve();
+      }
+      else {
+        reject(err);
+      }
+    });
+  });
+}

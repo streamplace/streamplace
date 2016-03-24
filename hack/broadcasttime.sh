40267ef936f66b5ce340bd627067a5e54a45d4a8
Eli Mallon <eli.mallon@csats.com>
2016-03-24 01:12:13 +0000

    Abstract out broadcasttime.sh

--- hack/broadcasttime.sh
+++ hack/broadcasttime.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Usage: hack/broadcasttime.sh URL TEXT
+# Url is rtmp://oregon.ingress.stream.kitchen:1934/stream/[this part here]
+
 curl -Lo fonts.tar.gz 'https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.tar.gz'
 tar xzf fonts.tar.gz
 mv "source-code-pro-2.010R-ro-1.030R-it/TTF/SourceCodePro-Semibold.ttf" font.ttf
 rm -rf "source-code-pro-2.010R-ro-1.030R-it" *.tar.gz
 
 drawTS="drawtext=fontfile=$(pwd)/font.ttf: text='%{localtime\:%H\\\\\\:%M\\\\\\:%S %z}': fontcolor=white@1: fontsize=50: x=50: y=25: box=1: boxcolor=black"
-drawName="drawtext=fontfile=$(pwd)/font.ttf: text='Frankfurt': fontcolor=white@1: fontsize=50: x=300: y=400: box=1: boxcolor=black"
+drawName="drawtext=fontfile=$(pwd)/font.ttf: text='$2': fontcolor=white@1: fontsize=50: x=300: y=400: box=1: boxcolor=black"
 
 ffmpeg \
   -f lavfi \
@@ -19,4 +22,4 @@ ffmpeg \
     [time]$drawName[output] \
   "\
   -map '[output]' \
-  'rtmp://oregon.ingress.stream.kitchen:1934/stream/frankfurt'
+  "rtmp://oregon.ingress.stream.kitchen:1934/stream/$1"

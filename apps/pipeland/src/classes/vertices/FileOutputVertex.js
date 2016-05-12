567e1018fe326341f023d6d101c5750c25059498
Eli Mallon <eli@stream.kitchen>
2016-05-12 00:45:20 +0000

    Implement multi-segment file upload

--- apps/pipeland/src/classes/vertices/FileOutputVertex.js
+++ apps/pipeland/src/classes/vertices/FileOutputVertex.js
@@ -1,5 +1,9 @@
 
 import AWS from "aws-sdk";
+import _ from "underscore";
+import leftPad from "left-pad";
+import {PassThrough} from "stream";
+import munger from "mpeg-munger";
 
 import BaseVertex from "./BaseVertex";
 import SK from "../../sk";
@@ -9,13 +13,13 @@ import ENV from "../../env";
 const dateStamp = function() {
   const now = new Date();
   const date = [ now.getFullYear(), now.getMonth() + 1, now.getDate() ];
+  return date.map((s) => leftPad(s, 2, 0)).join("-");
+};
+
+const timeStamp = function() {
+  const now = new Date();
   const time = [ now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds() ];
-  for ( let i = 1; i < time.length; i++ ) {
-    if ( time[i] < 10 ) {
-      time[i] = "0" + time[i];
-    }
-  }
-  return `${date.join("-")}-${time.join("-")}`;
+  return time.map((s) => leftPad(s, 2, 0)).join("-");
 };
 
 let s3Streamer;
@@ -27,12 +31,17 @@ if (ENV.AWS_ACCESS_KEY_ID && ENV.AWS_SECRET_ACCESS_KEY && ENV.AWS_USER_UPLOAD_BU
   });
 }
 
+const CHUNK_UPLOAD_INTERVAL = 5/* min*/ * 60/* sec*/ * 1000/*ms*/;
+
 export default class FileOutputVertex extends BaseVertex {
   constructor({id}) {
     if (!s3Streamer) {
       throw new Error("FileOutputVertex created, but missing required environment variables");
     }
     super({id});
+
+    this.uploads = [];
+    this.chunkIdx = 0;
     this.videoInputURL = this.transport.getInputURL();
     this.audioInputURL = this.transport.getInputURL();
     SK.vertices.update(id, {
@@ -56,35 +65,84 @@ export default class FileOutputVertex extends BaseVertex {
   }
 
   init() {
-    const prefix = `${dateStamp()}-${this.doc.title}`;
-    this.udpStreams = [];
+    this.started = false;
+    const date = dateStamp();
+    const time = timeStamp();
+    const folder = date.replace(/-/g, "/");
+    const prefix = `${folder}/${date}-${time}-${this.doc.title}`;
+
+    // Transport streams and upload streams have the same indices.
+    this.transportStreams = [];
+
+    this.count = 0;
     this.doc.inputs.forEach((input) => {
       input.sockets.forEach((socket) => {
-        const fileName = `${prefix}-${input.name}-${socket.type}.ts`;
-        this.info(`Uploading to ${fileName}`);
-        const udpStream = new this.transport.InputStream({url: socket.url});
-        const uploadStream = s3Streamer.upload({
-          Bucket: ENV.AWS_USER_UPLOAD_BUCKET,
-          Key: ENV.AWS_USER_UPLOAD_PREFIX + fileName,
-          Body: udpStream
-        });
-        this.udpStreams.push(udpStream);
-        uploadStream.on("httpUploadProgress", (data) => {
-          const kb = Math.floor(data.loaded/1024);
-          this.updateSelf({
-            status: "ACTIVE",
-            timemark: `${kb}k uploaded`
-          });
-        });
-        uploadStream.send();
+        const filePrefix = `${prefix}-${input.name}-${socket.type}`;
+        const transportStream = new this.transport.InputStream({url: socket.url});
+        const mpegStream = munger(); // Just to make sure we cut files at 188-byte intervals.
+        transportStream.pipe(mpegStream);
+        mpegStream.once("readable", this.startIdempotent.bind(this));
+        this.uploads.push({filePrefix, transportStream, mpegStream});
       });
     });
+    this.uploadNextChunk();
+  }
+
+  /**
+   * We don't want to sit around uploading empty files before we have the start of the stream. So,
+   * this function is run when we first see some data from the transport stream.
+   */
+  startIdempotent() {
+    if (!this.started) {
+      this.started = true;
+      this.uploadNextChunk();
+    }
+  }
+
+  uploadNextChunk() {
+    this.uploads.forEach((upload) => {
+      let {filePrefix, transportStream, uploadStream, mpegStream, passThroughStream} = upload;
+
+      // If we have an old upload stream, unpipe it.
+      if (passThroughStream) {
+        mpegStream.unpipe(passThroughStream);
+        passThroughStream.end();
+      }
+
+      // S3's "managed uploads" advertise that they work like streams (they have pipe and unpipe)
+      // but don't implement the full spec. Let's put a passthrough stream in front of it so we
+      // can treat it like one.
+      const fileName = `${filePrefix}-${leftPad(this.chunkIdx, 6, 0)}.ts`;
+      this.info(`Uploading to ${fileName}`);
+
+      upload.passThroughStream = PassThrough();
+      mpegStream.pipe(upload.passThroughStream);
+      upload.uploadStream = s3Streamer.upload({
+        Bucket: ENV.AWS_USER_UPLOAD_BUCKET,
+        Key: ENV.AWS_USER_UPLOAD_PREFIX + fileName,
+        Body: upload.passThroughStream
+      });
+      upload.uploadStream.on("httpUploadProgress", (data) => {
+        const kb = Math.floor(data.loaded/1024);
+        this.updateSelf({
+          status: "ACTIVE",
+          timemark: `${kb}k uploaded`
+        });
+      });
+      upload.uploadStream.send();
+    });
+    this.chunkIdx += 1;
+    this.chunkTimeoutHandle = setTimeout(this.uploadNextChunk.bind(this), CHUNK_UPLOAD_INTERVAL);
   }
 
   cleanup() {
     super.cleanup();
-    this.udpStreams.forEach((stream) => {
-      stream.stop();
+    if (this.chunkTimeoutHandle) {
+      clearTimeout(this.chunkTimeoutHandle);
+    }
+    this.transportStreams.forEach(({transportStream, passThroughStream}) => {
+      transportStream.stop();
+      passThroughStream.end();
     });
   }
 }

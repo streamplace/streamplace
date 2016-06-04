eb42a8e4d7f2d34002d2b73826653bcec9003a80
Eli Mallon <eli@stream.kitchen>
2016-06-04 03:15:04 +0000

    Remove old hacks

--- hack/inputtoudp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do
-  ffmpeg -i rtmp://oregon.ingress.stream.kitchen:1934/stream/iphone -f mpegts -an -c:v copy udp://localhost:40000
-  sleep 2
-done
--- hack/testvideo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ffmpeg \
-  -f lavfi -i "testsrc=size=1920x1080:rate=30" \
-  -f lavfi -i "sine=frequency=1000" \
-  -c:v libx264 \
-  -c:a aac \
-  -t 00:00:30 \
-  testvid.ts

2dd1bced2c242724d1dc83c128c5cef24ba5b650
Eli Mallon <eli@stream.kitchen>
2016-11-21 21:41:25 +0000

    Add necessary NODE_PATH

--- run/common.sh
+++ run/common.sh
@@ -4,6 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export NODE_PATH="$DIR/../apps"
+
 function bigPrint() {
   echo ""
   echo "===================================="

8be79dfd604d6d0d6786a448690135bb9d896c0b
Eli Mallon <eli@stream.kitchen>
2016-12-19 07:42:02 +0000

    Improve local kubernetes bootup

--- run/local-kube-start.sh
+++ run/local-kube-start.sh
@@ -5,23 +5,23 @@ set -o nounset
 set -o pipefail
 
 docker run \
-  --volume=/:/rootfs:ro \
-  --volume=/sys:/sys:ro \
-  --volume=/var/lib/docker/:/var/lib/docker:rw \
-  --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
-  --volume=/var/run:/var/run:rw \
   --net=host \
   --pid=host \
-  --privileged=true \
+  --privileged \
+  --volume=/:/rootfs:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/var/run:/var/run:rw \
+  --volume=/var/lib/docker/:/var/lib/docker:rw \
+  --volume=/var/lib/kubelet/:/var/lib/kubelet:shared \
   --name=kubelet \
   -d \
-  gcr.io/google_containers/hyperkube-amd64:v1.3.7 \
+  gcr.io/google_containers/hyperkube-amd64:v1.5.1 \
   /hyperkube kubelet \
+    --address="0.0.0.0" \
     --containerized \
     --hostname-override="127.0.0.1" \
-    --address="0.0.0.0" \
     --api-servers=http://localhost:8080 \
-    --config=/etc/kubernetes/manifests \
+    --pod-manifest-path=/etc/kubernetes/manifests \
     --cluster-dns=10.0.0.10 \
     --cluster-domain=cluster.local \
     --allow-privileged=true --v=2

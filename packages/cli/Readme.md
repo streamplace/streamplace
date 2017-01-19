2ca578f4feedb98a11bb3bcbee231cf2df66c125
Eli Mallon <eli@stream.kitchen>
2017-01-18 18:28:11 -0800

    basic cli

---
+++ packages/cli/Readme.md
@@ -0,0 +1,11 @@
+
+# cli
+
+This is the Streamplace command line interface.
+
+## Workin' on it
+
+```
+npm link
+npm run dev
+```
---
+++ packages/cli/src/sp.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+import program from "commander";
+import fs from "mz/fs";
+import {resolve, dirname} from "path";
+import pkg from "../package.json";
+import {safeLoad as readYaml, safeDump as writeYaml} from "js-yaml";
+import mkdirp from "mkdirp-promise";
+
+const configDefault = resolve(process.env.HOME, ".streamplace", "sp-config.yaml");
+
+const die = function(...args) {
+  console.error(...args);
+  process.exit(1);
+};
+
+const CONFIG_DEFAULT = {
+  authServer: "https://stream.place"
+};
+
+// const {version} = JSON.parse(pkg);
+export default program
+  .version(pkg.version)
+  .option("--sp-config <file>", "Location of sp-config.yaml", configDefault);
+
+const getConfig = function() {
+  const {spConfig} = program;
+  return fs.stat(spConfig)
+  .catch((err) => {
+    if (err.code !== "ENOENT") {
+      throw new Error(err);
+    }
+    return mkdirp(dirname(spConfig)).then(() => {
+      return fs.writeFile(spConfig, writeYaml(CONFIG_DEFAULT));
+    });
+  })
+  .then(() => {
+    return fs.readFile(spConfig, "utf8");
+  })
+  .then((data) => {
+    return readYaml(data);
+  });
+};
+
+program
+  .command("login", "authenticate with your streamplace dev server")
+  .action(function(command, env) {
+    getConfig(env.options)
+    .then((config) => {
+      console.log(config);
+      console.log("do login things now :)");
+    })
+    .catch(die);
+  });
+
+program.parse(process.argv);

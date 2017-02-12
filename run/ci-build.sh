8c4b678457446b7b1e0d70a2e68d422c5b925f1d
Eli Mallon <eli@stream.kitchen>
2017-02-12 05:10:34 +0000

    ci tweaks

--- run/ci-build.sh
+++ run/ci-build.sh
@@ -3,7 +3,6 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
@@ -13,13 +12,13 @@ THIS_IS_CI="${THIS_IS_CI:-}"
 
 # This means we're authorized to build some docker images
 if [[ $THIS_IS_CI == "true" ]]; then
+  apt-get update && apt-get install -y realpath jq curl
   mkdir -p ~/.docker
   echo "$DOCKER_CONFIG" | base64 --decode > ~/.docker/config.json
   echo "$NPMRC" | base64 --decode > ~/.npmrc
   docker run \
     -e FIX_OR_ERROR="FIX" \
     -v ~/.docker/config.json:/root/.docker/config.json \
-    -v /var/run/docker.sock:/var/run/docker.sock \
     -v ~/.npmrc:/root/.npmrc \
     -v "$DIR":/build streamplace/sp-dev:latest /build/run/full-build.sh
 fi

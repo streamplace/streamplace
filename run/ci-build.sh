2fc07ea54708e90552ca87f40672508ebe6ddb11
Eli Mallon <eli@stream.kitchen>
2017-02-12 04:46:56 +0000

    Looks like docker full build is working

--- run/ci-build.sh
+++ run/ci-build.sh
@@ -6,4 +6,19 @@ set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-docker run --rm -e FIX_OR_ERROR="ERR" -v "$DIR":/build streamplace/sp-dev:latest /build/run/ci-build-container.sh
+docker run --rm -e FIX_OR_ERROR="ERR" -v "$DIR":/build streamplace/sp-dev:latest /build/run/lint.sh
+
+THIS_IS_CI="${THIS_IS_CI:-}"
+
+# This means we're authorized to build some docker images
+if [[ $THIS_IS_CI == "true" ]]; then
+  mkdir -p ~/.docker
+  echo "$DOCKER_CONFIG" | base64 --decode > ~/.docker/config.json
+  echo "$NPMRC" | base64 --decode > ~/.npmrc
+  docker run --rm \
+    -e FIX_OR_ERROR="FIX" \
+    -v ~/.docker/config.json:/root/.docker/config.json \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ~/.npmrc:/root/.npmrc \
+    -v "$DIR":/build streamplace/sp-dev:latest /build/run/full-build.sh
+fi
--- run/full-build.sh
+++ run/full-build.sh
@@ -5,10 +5,10 @@ set -o nounset
 set -o pipefail
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+source "$ROOT/run/common.sh"
 
 cd "$ROOT"
 npm install
-npm run lint
 
 # Linted. Cool, we're good to deploy.
 gitDescribe=$(git describe --tags)
@@ -19,7 +19,7 @@ lerna publish --skip-git --skip-npm --yes --repo-version "$repoVersion"
 # and now run the linting script that updates all the Chart.yaml files
 FIX_OR_ERR="FIX" lerna exec $(realpath "$ROOT/run/package-lint.sh")
 # Cool. With that, we're good to build. First publish the new version of the npm packages...
-# lerna publish --skip-git --yes --repo-version "$repoVersion"
+lerna publish --skip-git --yes --repo-version "$repoVersion"
 # Sweet, time for Docker!
 lerna exec --concurrency=1 $(realpath "$ROOT/run/package-docker-build.sh")
 lerna exec --concurrency=1 $(realpath "$ROOT/run/package-docker-push.sh")

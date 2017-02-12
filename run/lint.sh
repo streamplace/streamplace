c71e1b30846500b8b245d0d72ea1a102eeb86d00
Eli Mallon <eli@stream.kitchen>
2017-02-12 21:33:06 +0000

    npm install

--- run/lint.sh
+++ run/lint.sh
@@ -7,7 +7,11 @@ set -o pipefail
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 source "$ROOT/run/common.sh"
 
-export FIX_OR_ERR=${FIX_OR_ERR:-FIX}
 cd "$ROOT"
+if [[ $THIS_IS_CI == "true" ]]; then
+  npm install
+fi
+
+export FIX_OR_ERR=${FIX_OR_ERR:-FIX}
 eslint --color --ext=jsx --ext=js .
 lerna --concurrency=1 exec "$ROOT/run/package-lint.sh"

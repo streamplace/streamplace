7021df726e718c2fa1bdd101fb1f3c0f0a770d7e
Eli Mallon <eli@stream.kitchen>
2017-02-13 02:14:05 +0000

    Sync package.json version with everyone else's

--- run/lint.sh
+++ run/lint.sh
@@ -13,5 +13,15 @@ if [[ $THIS_IS_CI == "true" ]]; then
 fi
 
 export FIX_OR_ERR=${FIX_OR_ERR:-FIX}
+
+newPackage="$(cat package.json)"
+correctVersion="$(cat "$ROOT/lerna.json" | jq -r '.version')"
+packageVersion="$(cat "$ROOT/package.json" | jq -r '.version')"
+if [[ "$packageVersion" != "$correctVersion" ]]; then
+  fixOrErr "top-level package.json has version $packageVersion instead of $correctVersion"
+  newPackage=$(tweak "$newPackage" ".version" "$correctVersion")
+  echo "$newPackage" > package.json
+fi
+
 eslint --color --ext=jsx --ext=js .
 lerna --concurrency=1 exec "$ROOT/run/package-lint.sh"

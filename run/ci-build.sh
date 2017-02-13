ede573c78acfc0a630772be545509579cff672b7
Eli Mallon <eli@stream.kitchen>
2017-02-12 22:51:37 -0800

    don't clobber my system npm and config.json plz

--- run/ci-build.sh
+++ run/ci-build.sh
@@ -13,19 +13,23 @@ docker run \
   -e THIS_IS_CI="$THIS_IS_CI" \
   -v "$ROOT":/build streamplace/sp-dev:latest /build/run/lint.sh
 
+tmp="$ROOT/tmp/$(date +%s)"
+mkdir -p $tmp
+
 # This means we're authorized to build some docker images
 if [[ $THIS_IS_CI == "true" ]]; then
-  mkdir -p ~/.docker
-  echo "$DOCKER_CONFIG_JSON" | base64 --decode > ~/.docker/config.json
-  echo "$NPMRC" | base64 --decode > ~/.npmrc
+  echo "$DOCKER_CONFIG_JSON" | base64 --decode > "$tmp/config.json"
+  echo "$NPMRC" | base64 --decode > "$tmp/npmrc"
   docker run \
     -e FIX_OR_ERROR="FIX" \
     -e NPM_CONFIG_LOGLEVEL="warn" \
     -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
     -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
     -e AWS_DEFAULT_REGION="us-west-2" \
-    -v ~/.docker/config.json:/root/.docker/config.json \
+    -v "$tmp/config.json":/root/.docker/config.json \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v ~/.npmrc:/root/.npmrc \
+    -v "$tmp/npmrc":/root/.npmrc \
     -v "$ROOT":/build streamplace/sp-dev:latest /build/run/full-build.sh
 fi
+
+rm -rf "$ROOT/tmp/*"

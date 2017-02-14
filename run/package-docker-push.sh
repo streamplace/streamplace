8f520db791b97dc515f6fd42c5eec323c2d26171
Eli Mallon <eli@stream.kitchen>
2017-02-14 12:46:21 -0800

    Add npm run docker-build

--- run/package-docker-push.sh
+++ run/package-docker-push.sh
@@ -11,5 +11,10 @@ if [[ ! -f Dockerfile ]]; then
   exit 0
 fi
 
+beforeContainer="streamplace/$PACKAGE_NAME:latest"
 taggedContainer="$DOCKER_PREFIX/$PACKAGE_NAME:v$repoVersion"
+
+# Tag later, so the streamplace/whatever tag can be used to build other images.
+info "Tagging $beforeContainer as $taggedContainer"
+docker tag $beforeContainer $taggedContainer
 docker push $taggedContainer

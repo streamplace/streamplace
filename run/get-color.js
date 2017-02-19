569021665cec62e0ee1c6b12095d0150c187068d
Eli Mallon <eli@stream.place>
2017-02-18 17:06:27 -0800

    reduce unnecessary hashing

--- run/get-color.js
+++ run/get-color.js
@@ -10,13 +10,19 @@ const BLACKLIST = [
   16, 17, 18, 232, 233, 234, 235, 236, 237, 238, 239
 ];
 
+const memoized = {};
+
 module.exports = function(input) {
+  if (memoized[input]) {
+    return memoized[input];
+  }
   let color;
 
   while (!color || BLACKLIST.indexOf(color) !== -1) {
     input = crypto.createHash("sha1").update(input).digest("hex");
     color = parseInt(input.slice(8)) % 255;
   }
+  memoized[input] = color;
   return color;
 };
 

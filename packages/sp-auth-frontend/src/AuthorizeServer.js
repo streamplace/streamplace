7d2a4a5af355014913ca145d1d1ee4d4897c4107
Eli Mallon <eli@iame.li>
2017-03-09 19:51:42 -0800

    Make nicer looking login page

--- packages/sp-auth-frontend/src/AuthorizeServer.js
+++ packages/sp-auth-frontend/src/AuthorizeServer.js
@@ -1,6 +1,74 @@
 
 import React from "react";
 import SP from "sp-client";
+import styled from "styled-components";
+import color from "color";
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  color: white;
+  font-size: 1.3em;
+  -webkit-font-smoothing: antialiased;
+`;
+
+const Emphasis = styled.span`
+  color: #ffcf00;
+  display: block;
+  margin: 0.5em;
+`;
+
+const Centered = styled.div`
+  margin: auto;
+  width: 500px;
+  text-align: center;
+  padding: 1em;
+`;
+
+const Big = styled.h1`
+  font-weight: 200;
+`;
+
+const Aside = styled.aside`
+  font-style: oblique;
+  margin: 1.5em auto;
+  line-height: 1.2em;
+`;
+
+const Button = styled.button`
+  font-size: 1.3em;
+  background-color: transparent;
+  padding: 0.7em 1.2em;
+  margin: 1em;
+  border: 1px solid black;
+  cursor: pointer;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const lighter = color("#ffcf00").lighten(0.5).toString();
+
+const GoodButton = styled(Button)`
+  color: black;
+  border-color: #ffcf00;
+  background-color: #ffcf00;
+  color: #333;
+  &:hover, &:focus {
+    background-color: ${lighter};
+  }
+`;
+
+// formerly #ff2d2d
+const BadButton = styled(Button)`
+  color: #ffcf00;
+  border-color: #ffcf00;
+  &:hover, &:focus {
+    background-color: #555;
+    color: ${lighter};
+    border-color: ${lighter};
+  }
+`;
 
 // If you're working on this, this snippet will likely be helpful.
 // SP.serverauths.find().then(auths => auths.forEach(auth => SP.serverauths.delete(auth.id)))
@@ -67,7 +135,7 @@ export default class AuthorizeServer extends React.Component{
     window.location = `${this.state.returnUrl}?authRejected=true`;
   }
 
-  render () {
+  renderInner () {
     if (!this.state.ready) {
       return <div>Loading...</div>;
     }
@@ -80,11 +148,23 @@ export default class AuthorizeServer extends React.Component{
     }
     return (
       <div>
-        <div>Log in to {this.state.server} using your Streamplace account?</div>
-        <aside>(All this means is that they get to see your email address. If we need other permissions, we'll ask again later.)</aside>
-        <button onClick={this.handleYeah.bind(this)}>Yeah</button>
-        <button onClick={this.handleNah.bind(this)}>Nah</button>
+        <Big>
+          Log in to
+          <Emphasis>{this.state.server}</Emphasis>
+          using your Streamplace account?
+        </Big>
+        <Aside>All this means is that they get to see your email address. If we need other permissions, we'll ask again later.</Aside>
+        <GoodButton onClick={this.handleYeah.bind(this)}>Yeah</GoodButton>
+        <BadButton onClick={this.handleNah.bind(this)}>Nah</BadButton>
       </div>
+    );
+  }
+
+  render() {
+    return (
+      <Container>
+        <Centered>{this.renderInner()}</Centered>
+      </Container>
     );
   }
 }

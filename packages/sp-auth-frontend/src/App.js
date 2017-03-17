8bd897491359b3522b7975be0bb1fd35213633eb
Eli Mallon <eli@iame.li>
2017-03-17 14:43:56 -0700

    Improve auth process
    It now uses redirects instead of iframes.

--- packages/sp-auth-frontend/src/App.js
+++ packages/sp-auth-frontend/src/App.js
@@ -27,7 +27,6 @@ class App extends Component {
       parentOrigin: `https://${query.server || "stream.place"}`,
     };
     if (this.state.logout) {
-      window.localStorage.removeItem("SP_AUTH_TOKEN");
       window.location = `https://${this.state.server}${this.state.returnPath}`;
       return;
     }
@@ -57,19 +56,16 @@ class App extends Component {
 
   componentWillMount() {
     if (this.state.logout) {
-      window.localStorage.removeItem("SP_AUTH_TOKEN");
       return;
     }
     // This SPClient might not succeed in connection to the server 'cuz we're the login page, but
     // that's fine because we're just using it to get the schema.
-    const token = window.localStorage.getItem("SP_AUTH_TOKEN");
-    SP.connect({token})
+    SP.connect()
     .then((user) => {
       this.setState({phase: LOGGED_IN});
     })
     .catch((err) => {
       this.setState({phase: LOGGED_OUT});
-      window.localStorage.removeItem("SP_AUTH_TOKEN");
     });
   }
 
@@ -78,11 +74,10 @@ class App extends Component {
   }
 
   handleLogin(token) {
+    this.setState({phase: START});
     SP.connect({token})
     .then((user) => {
       this.setState({phase: LOGGED_IN});
-      // Not going with this for now. Tokens persist on clients.
-      // window.localStorage.setItem("SP_AUTH_TOKEN", SP.token);
     })
     .catch((err) => {
       SP.error(err);
@@ -91,13 +86,7 @@ class App extends Component {
   }
 
   handleToken(token) {
-    this.tellParent({
-      type: "token",
-      token: token,
-    })
-    .then(() => {
-      window.close();
-    });
+    window.location = `https://${this.state.server}${this.state.returnPath}?${qs.stringify({token})}`;
   }
 
   handleRejected() {
@@ -106,7 +95,7 @@ class App extends Component {
 
   render() {
     if (this.state.phase === START) {
-      return <div>Loading</div>;
+      return <div></div>;
     }
     else if (this.state.phase === LOGGED_IN) {
       return <AuthorizeServer

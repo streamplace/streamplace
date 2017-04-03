b0150376de8d8fe30ac5a6ebc054aab2d073649e
Eli Mallon <eli@stream.place>
2017-04-03 03:28:17 -0700

    auto-join channels

--- packages/sp-frontend/src/channel.js
+++ packages/sp-frontend/src/channel.js
@@ -10,9 +10,6 @@ import {TitleBar, ChannelName, CanvasWrapper} from "./channel.style";
 export class Channel extends Component {
   static propTypes = {
     channel: React.PropTypes.object,
-  };
-
-  static contextTypes = {
     SP: React.PropTypes.object.isRequired,
   };
 
@@ -26,13 +23,46 @@ export class Channel extends Component {
   constructor() {
     super();
     this.state = {};
+    this.isAdding = false;
+  }
+
+  componentWillUnmount() {
+    this.isAdding = true;
+    const {channel, SP} = this.props;
+    SP.channels.update(channel.id, {
+      users: channel.users.filter(u => u.userId !== SP.user.id)
+    })
+    .catch((err) => {
+      SP.error(err);
+    });
   }
 
   render () {
-    const channel = this.props.channel;
+    const {channel, SP} = this.props;
     if (!channel || !channel.activeSceneId) {
       return <FlexContainer />;
     }
+
+    // For now... if you're not in a channel, auto-join yourself.
+    if (!this.isAdding && !channel.users.find(u => u.userId === SP.user.id)) {
+      this.isAdding = true;
+      SP.channels.update(channel.id, {
+        users: [
+          ...channel.users,
+          {
+            userId: SP.user.id,
+            muted: false,
+          }
+        ]
+      })
+      .then(() => {
+        this.isAdding = false;
+      })
+      .catch((err) => {
+        this.isAdding = false;
+      });
+    }
+
     return (
       <FlexContainer>
         <TitleBar>
@@ -40,7 +70,7 @@ export class Channel extends Component {
         </TitleBar>
         <FlexContainer>
           <CanvasWrapper>
-            <SPCanvas width={1920} height={1080}>
+            <SPCanvas className="main" width={1920} height={1080}>
               <SPScene sceneId={channel.activeSceneId} />
             </SPCanvas>
           </CanvasWrapper>

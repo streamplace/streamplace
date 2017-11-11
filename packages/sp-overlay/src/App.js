5bffe607d04b5a2d39dc79fbf88a567e36051a36
Eli Mallon <eli@stream.place>
2017-11-11 00:58:06 -0800

    better github logo

--- packages/sp-overlay/src/App.js
+++ packages/sp-overlay/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
         <a href="https://github.com/streamplace/streamplace">
           <img
             style={{ position: "absolute", top: 0, left: 0, border: 0 }}
-            src="https://camo.githubusercontent.com/c6286ade715e9bea433b4705870de482a654f78a/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f77686974655f6666666666662e706e67"
+            src="https://cdn.rawgit.com/tholman/github-corners/0a2c2767/svg/github-corner-left.svg"
             alt="Fork me on GitHub"
             data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_white_ffffff.png"
           />

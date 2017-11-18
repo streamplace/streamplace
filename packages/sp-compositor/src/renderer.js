1de1fc43fbb0ae7594542fd259239b7f50e2daf2
Eli Mallon <eli@stream.place>
2017-11-17 17:36:31 -0800

    Woo, video output!

--- packages/sp-compositor/src/renderer.js
+++ packages/sp-compositor/src/renderer.js
@@ -1,11 +1,17 @@
 const { desktopCapturer } = require("electron");
 const url = require("url");
 const querystring = require("querystring");
+const fs = require("fs");
+const path = require("path");
 
 const options = querystring.parse(url.parse(document.location.href).query);
 const video = document.querySelector("video");
 video.style.width = `${options.width}px`;
 video.style.height = `${options.height}px`;
+
+const writeStream = fs.createWriteStream(
+  path.resolve(__dirname, "..", `${options.windowId}.webm`)
+);
 
 desktopCapturer.getSources(
   { types: ["window", "screen"] },
@@ -27,7 +33,22 @@ desktopCapturer.getSources(
         }
       })
       .then(stream => {
-        video.src = URL.createObjectURL(stream);
+        // video.srcObject = stream;
+        const recorder = new MediaRecorder(stream);
+        window.recorder = recorder;
+        recorder.ondataavailable = event => {
+          var fileReader = new FileReader();
+          fileReader.onload = function() {
+            writeStream.write(Buffer.from(this.result));
+          };
+          fileReader.readAsArrayBuffer(event.data);
+        };
+        recorder.onerror = (...args) => console.log("onerror", args);
+        recorder.onpause = (...args) => console.log("onpause", args);
+        recorder.onresume = (...args) => console.log("onresume", args);
+        recorder.onstart = (...args) => console.log("onstart", args);
+        recorder.onstop = (...args) => console.log("onstop", args);
+        recorder.start(100);
       })
       .catch(err => {
         console.log("getUserMedia error", err);

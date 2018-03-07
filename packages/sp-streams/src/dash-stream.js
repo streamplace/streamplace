474471aa116edcd7afa7be5c4c6b8a6524de16a9
Eli Mallon <eli@stream.place>
2018-03-06 21:14:53 -0800

    make ugly ffmpeg hack even uglier

--- packages/sp-streams/src/dash-stream.js
+++ packages/sp-streams/src/dash-stream.js
@@ -46,55 +46,69 @@ export default function dashStream(opts = {}) {
     }
   });
 
-  const listener = app.listen(0, () => {
-    socketEgress.httpPort = listener.address().port;
-    log(`DASH HTTP ingress listening on port ${socketEgress.httpPort}`);
-
-    ffmpeg = ffmpegCreate()
-      .input(`unix://${socketEgress.path}`)
-      .inputFormat("mpegts")
-      .videoCodec("copy")
-      .audioCodec("copy")
-      .inputOptions(["-probesize 60000000", "-analyzeduration 10000000"])
-      .outputOptions([
-        // This section from default options at https://ffmpeg.org/ffmpeg-all.html#dash-2
-        "-bf 1",
-        "-keyint_min 120",
-        "-g 120",
-        "-sc_threshold 0",
-        "-b_strategy 0",
-        "-ar:a:1 22050",
-        "-use_timeline 1",
-        "-use_template 1",
-        // hls too!
-        "-hls_playlist 1",
-        // Avoids Tag [15][0][0][0] incompatible with output codec id '86018' (mp4a)
-        "-tag:v avc1",
-        "-tag:a mp4a",
-        `-window_size ${passThrough.windowSize}`,
-        `-min_seg_duration ${passThrough.segDuration * 1000}` // ms ==> microseconds
-      ])
-      .output(`http://127.0.0.1:${socketEgress.httpPort}/${MANIFEST_NAME}`)
-      .outputFormat("dash");
-    // hack hack hack, but for now we need a newer version
-    const FFMPEG_UNSTABLE_PATH = "/usr/bin/ffmpeg-unstable";
-    if (fs.existsSync(FFMPEG_UNSTABLE_PATH)) {
-      ffmpeg.setFfmpegPath(FFMPEG_UNSTABLE_PATH);
-    }
-  });
-
   passThrough.on("end", () => {
     listener.close(() => {
       log(`DASH HTTP ingress closed on port ${socketEgress.httpPort}`);
     });
   });
 
-  passThrough.getPath = socketEgress.getPath.bind(socketEgress);
+  let listener;
 
-  socketEgress.getPath().then(() => {
-    passThrough.pipe(socketEgress);
-    ffmpeg.run();
-  });
+  Promise.resolve()
+    .then((resolve, reject) => {
+      listener = app.listen(0, resolve);
+    })
+    .then(() => {
+      socketEgress.httpPort = listener.address().port;
+      log(`DASH HTTP ingress listening on port ${socketEgress.httpPort}`);
+
+      ffmpeg = ffmpegCreate()
+        .input(`unix://${socketEgress.path}`)
+        .inputFormat("mpegts")
+        .videoCodec("copy")
+        .audioCodec("copy")
+        .inputOptions(["-probesize 60000000", "-analyzeduration 10000000"])
+        .outputOptions([
+          // This section from default options at https://ffmpeg.org/ffmpeg-all.html#dash-2
+          "-bf 1",
+          "-keyint_min 120",
+          "-g 120",
+          "-sc_threshold 0",
+          "-b_strategy 0",
+          "-ar:a:1 22050",
+          "-use_timeline 1",
+          "-use_template 1",
+          // hls too!
+          "-hls_playlist 1",
+          // Avoids Tag [15][0][0][0] incompatible with output codec id '86018' (mp4a)
+          "-tag:v avc1",
+          "-tag:a mp4a",
+          `-window_size ${passThrough.windowSize}`,
+          `-min_seg_duration ${passThrough.segDuration * 1000}` // ms ==> microseconds
+        ])
+        .output(`http://127.0.0.1:${socketEgress.httpPort}/${MANIFEST_NAME}`)
+        .outputFormat("dash");
+      return socketEgress.getPath();
+    })
+    .then(() => {
+      passThrough.pipe(socketEgress);
+      // hack hack hack, but for now we need a newer version
+      const FFMPEG_UNSTABLE_PATH = "/usr/bin/ffmpeg-unstable";
+      if (!fs.existsSync(FFMPEG_UNSTABLE_PATH)) {
+        throw new Error("ffmpeg-unstable not found");
+      } else {
+        log(`using ${FFMPEG_UNSTABLE_PATH}`);
+        // ugh, this is a global setting for some reason
+        ffmpeg._getFfmpegPath = cb => cb(null, FFMPEG_UNSTABLE_PATH);
+        ffmpeg.run();
+      }
+    })
+    .catch(err => {
+      log(err);
+      throw err;
+    });
+
+  passThrough.getPath = socketEgress.getPath.bind(socketEgress);
 
   return passThrough;
 }

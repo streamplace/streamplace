23831730a9eefb6f3fc3bb5393c2ef728d1d6fcc
Eli Mallon <eli@stream.place>
2018-03-21 22:30:23 -0700

    login page mockup is done

--- packages/streamplace-ui/src/streamplace-ui.js
+++ packages/streamplace-ui/src/streamplace-ui.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Modal,
+  TouchableHighlight,
+  TextInput,
+  Image,
+  Button,
+  Linking
+} from "react-native";
 import styled, { injectGlobal } from "styled-components";
 
-if (typeof navigator.userAgent !== "undefined") {
+const IS_NATIVE = typeof navigator.userAgent === "undefined";
+
+if (!IS_NATIVE) {
   injectGlobal`
     html,
     body,
@@ -15,31 +27,89 @@ if (typeof navigator.userAgent !== "undefined") {
   `;
 }
 
-// const Centered = (styled.View || styled(View))`
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   height: 100%;
-// `;
+if (!styled.View) {
+  styled.View = styled(View);
+}
+if (!styled.TextInput) {
+  styled.TextInput = styled(TextInput);
+}
+if (!styled.Image) {
+  styled.Image = styled(Image);
+}
+
+const Overall = styled.View`
+  align-items: center;
+  justify-content: flex-start;
+  flex-grow: 1;
+  padding: 15px;
+`;
+
+const RestCentered = styled.View`
+  align-items: stretch;
+  max-width: 550px;
+  justify-content: flex-start;
+  flex-grow: 1;
+  width: 100%;
+`;
+
+const UserName = styled.TextInput`
+  border-bottom-color: black;
+  border-bottom-width: 1px;
+  border-style: solid;
+  height: 80px;
+  font-size: 30px;
+  width: 100%;
+  margin-bottom: 10px;
+`;
+
+const LogoImage = styled.Image`
+  height: 200px;
+  width: 100%;
+`;
+
+let logoSource = require("./streamplace-logo.svg");
+if (IS_NATIVE) {
+  logoSource = require("./streamplace-logo.png");
+}
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: ""
+    };
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{`${typeof navigator.userAgent === "undefined"}`}</Text>
-        <Text>hello hi woo</Text>
-        <Text>hello hi woo</Text>
-        <Text>hello hi woo</Text>
-      </View>
+      <Overall>
+        <RestCentered>
+          <LogoImage resizeMode={"contain"} source={logoSource} />
+          <UserName
+            onChangeText={text =>
+              this.setState({
+                value: text
+              })
+            }
+            editable={true}
+            value={this.state.value}
+            placeholder="eli@iame.li"
+          />
+          <UserName
+            onChangeText={text =>
+              this.setState({
+                value: text
+              })
+            }
+            editable={true}
+            value={this.state.value}
+            placeholder="**********"
+          />
+          <Button
+            title="Log In"
+            onPress={() => Linking.openURL("https://stream.place/")}
+          />
+        </RestCentered>
+      </Overall>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});

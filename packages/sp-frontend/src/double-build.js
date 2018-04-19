3b539a0076bce0ea9493684cf01357dc3c543d20
Eli Mallon <eli@stream.place>
2018-04-19 15:28:33 -0700

    put double-build.js in src so docker has it

--- packages/sp-frontend/src/double-build.js
+++ packages/sp-frontend/src/double-build.js
@@ -11,24 +11,25 @@ const child = require("child_process");
 const fs = require("fs-extra");
 const path = require("path");
 
+const pkgRoot = path.resolve(__dirname, "..");
 const oldPkgStr = fs.readFileSync(
-  path.resolve(__dirname, "package.json"),
+  path.resolve(pkgRoot, "package.json"),
   "utf8"
 );
 
-fs.removeSync(path.resolve(__dirname, "build"));
-fs.removeSync((__dirname, "build-electron"));
+fs.removeSync(path.resolve(pkgRoot, "build"));
+fs.removeSync((pkgRoot, "build-electron"));
 const newPkg = { ...JSON.parse(oldPkgStr), homepage: "." };
 fs.writeFileSync(
-  path.resolve(__dirname, "package.json"),
+  path.resolve(pkgRoot, "package.json"),
   JSON.stringify(newPkg, null, 2)
 );
 console.log("++++ building sp-frontend for electron ++++");
-child.execSync("npm run prepare:build", { cwd: __dirname, stdio: "inherit" });
+child.execSync("npm run prepare:build", { cwd: pkgRoot, stdio: "inherit" });
 fs.renameSync(
-  path.resolve(__dirname, "build"),
-  path.resolve(__dirname, "build-electron")
+  path.resolve(pkgRoot, "build"),
+  path.resolve(pkgRoot, "build-electron")
 );
-fs.writeFileSync(path.resolve(__dirname, "package.json"), oldPkgStr);
+fs.writeFileSync(path.resolve(pkgRoot, "package.json"), oldPkgStr);
 console.log("++++ building sp-frontend for browsers ++++");
-child.execSync("npm run prepare:build", { cwd: __dirname, stdio: "inherit" });
+child.execSync("npm run prepare:build", { cwd: pkgRoot, stdio: "inherit" });

7054d2a271f9232d6ece4fb7ba278a38f79b5cbd
Eli Mallon <eli@aquareum.tv>
2024-12-03 15:38:23 -0800

    meson: use mediamtx as entrypoint

--- cmd/libaquareum/aquareum.go
+++ cmd/libaquareum/aquareum.go
@@ -1,19 +1,28 @@
+// entrypoint for aquareum + mediamtx
 package main
 
 import (
+	"os"
+
 	"context"
 	"strconv"
 
+	"aquareum.tv/aquareum/pkg/cmd"
 	"aquareum.tv/aquareum/pkg/config"
 	"aquareum.tv/aquareum/pkg/log"
-
-	"aquareum.tv/aquareum/pkg/cmd"
-	// _ "github.com/go-gst/go-glib/glib"
-	// _ "github.com/go-gst/go-gst/gst"
+	"github.com/bluenviron/mediamtx/internal/core"
 )
 
-//#cgo LDFLAGS: -lz
 import "C"
+
+//export MTXMain
+func MTXMain() {
+	s, ok := core.New(os.Args[2:])
+	if !ok {
+		os.Exit(1)
+	}
+	s.Wait()
+}
 
 //export AquareumMain
 func AquareumMain() {
@@ -29,8 +38,4 @@ func AquareumMain() {
 	if err != nil {
 		log.Log(context.Background(), "exited uncleanly", "error", err)
 	}
-}
-
-func main() {
-	AquareumMain()
 }

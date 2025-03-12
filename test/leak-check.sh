7b41b17fa3f128fac02903ef25eb29410ab57f37
Eli Mallon <eli@aquareum.tv>
2025-03-12 15:18:20 -0700

    testing: tweaks to test/leak-check

--- test/leak-check.sh
+++ test/leak-check.sh
@@ -8,12 +8,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 TMPDIR="$(mktemp -d)"
 cd $TMPDIR
 
-MALLOC_CONF=prof_leak:true,lg_prof_sample:0,prof_final:true LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.2 "$DIR/build-linux-amd64/streamplace" --no-firehose --wide-open &
+MALLOC_CONF=prof_leak:true,lg_prof_sample:0,prof_final:true LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.2 \
+  "$DIR/build-linux-amd64/streamplace" --no-firehose --wide-open &
 STREAMPLACE_PID=$!
 
 sleep 3
 
-"$DIR/build-linux-amd64/streamplace" whip --count=3 --duration=30s || true
+"$DIR/build-linux-amd64/streamplace" whip --count=3 --duration=90s \
+  --file=$HOME/testvids/RocketLeague_1h55m_1sGOP_1080p60_NoBframes.mp4 || true
 
 sleep 5
 curl -X POST http://127.0.0.1:39090/gc

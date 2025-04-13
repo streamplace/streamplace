e943f8ec656a368460e59b450b515faf183bbd1b
Eli Mallon <eli@aquareum.tv>
2025-04-13 13:52:35 -0700

    lol

--- docker/bunny.sh
+++ docker/bunny.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-RUN echo "downloading $curl -o /usr/bin/streamplace $STREAMPLACE_URL_LINUX_AMD64" && cd /usr/local/bin && curl -L "$curl -o /usr/bin/streamplace $STREAMPLACE_URL_LINUX_AMD64" | tar xzv
+echo "downloading $STREAMPLACE_URL_LINUX_AMD64" && cd /usr/local/bin && curl -L "$STREAMPLACE_URL_LINUX_AMD64" | tar xzv
 chmod +x /usr/bin/streamplace
 
 exec /usr/bin/streamplace

87c71b220426f133fc7ab57dd9bf86d3a021fa71
Eli Mallon <eli@aquareum.tv>
2025-04-17 14:38:44 -0700

    media: move leak test to its own file

---
+++ pkg/media/leak_test.go
@@ -0,0 +1,133 @@
+package media
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"runtime/debug"
+	"strings"
+	"sync"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/acarl005/stripansi"
+	"github.com/stretchr/testify/require"
+)
+
+const LEAK_LINE = "GST_TRACER :0:: object-alive"
+
+var LEAK_DONE_REGEX = regexp.MustCompile(`listed\s+(\d+)\s+alive\s+objects`)
+
+var LeakReport = []string{}
+var LeakReportMutex sync.Mutex
+var LeakDoneCh = make(chan struct{})
+
+func TestMain(m *testing.M) {
+	gstDebug := os.Getenv("GST_DEBUG")
+	if gstDebug == "" {
+		gstDebug = "GST_TRACER:9"
+	} else {
+		gstDebug = fmt.Sprintf("%s,GST_TRACER:7", gstDebug)
+	}
+	os.Setenv("GST_DEBUG", gstDebug)
+	os.Setenv("GST_TRACERS", "leaks")
+	os.Setenv("GST_LEAKS_TRACER_SIG", "1")
+	debug.SetGCPercent(5)
+
+	f, err := os.MkdirTemp("", "")
+	if err != nil {
+		panic(err)
+	}
+	fName := filepath.Join(f, "leak.log")
+	err = syscall.Mkfifo(fName, 0640)
+	if err != nil {
+		panic(err)
+	}
+	os.Setenv("GST_DEBUG_FILE", fName)
+
+	go func() {
+		pipe, err := os.OpenFile(fName, os.O_RDONLY, 0640)
+		if err != nil {
+			panic(err)
+		}
+		defer pipe.Close()
+		// Read and print each line from FD
+		scanner := bufio.NewScanner(pipe)
+		for scanner.Scan() {
+			line := scanner.Text()
+			line = stripansi.Strip(line)
+			if strings.Contains(line, LEAK_LINE) {
+				LeakReportMutex.Lock()
+				LeakReport = append(LeakReport, line)
+				LeakReportMutex.Unlock()
+			} else if LEAK_DONE_REGEX.MatchString(line) {
+				LeakDoneCh <- struct{}{}
+			} else {
+				fmt.Println(line)
+				continue
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			panic(err)
+		}
+	}()
+	os.Exit(m.Run())
+}
+
+func checkGStreamerLeaks(t *testing.T, expected int) {
+	process, err := os.FindProcess(os.Getpid())
+	err = process.Signal(os.Signal(syscall.SIGUSR1))
+	require.NoError(t, err)
+	<-LeakDoneCh
+	LeakReportMutex.Lock()
+	before := len(LeakReport)
+	LeakReport = []string{}
+	LeakReportMutex.Unlock()
+	require.NoError(t, err)
+
+	ch := make(chan struct{})
+	done := false
+
+	go func() {
+		thing := &[]byte{}
+		runtime.SetFinalizer(thing, func(thing *[]byte) {
+			done = true
+			ch <- struct{}{}
+		})
+	}()
+
+	go func() {
+		runtime.GC()
+		runtime.GC()
+		for {
+			if done {
+				break
+			}
+			runtime.GC()
+			runtime.GC()
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+
+	<-ch
+	time.Sleep(1 * time.Second)
+
+	err = process.Signal(os.Signal(syscall.SIGUSR1))
+	require.NoError(t, err)
+
+	<-LeakDoneCh
+
+	LeakReportMutex.Lock()
+	after := len(LeakReport)
+	if after-before > expected {
+		for _, l := range LeakReport {
+			fmt.Println(l)
+		}
+	}
+	require.Equal(t, expected, len(LeakReport), "Leaks found")
+	LeakReportMutex.Unlock()
+}

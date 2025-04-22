cb39151f70db442b75c5a09716fba9da013d8242
Eli Mallon <eli@aquareum.tv>
2025-04-22 13:19:54 -0700

    media: zero-leak writerNewSample test

--- pkg/media/io_helpers_test.go
+++ pkg/media/io_helpers_test.go
@@ -1,13 +1,16 @@
 package media
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"testing"
 
 	"github.com/go-gst/go-gst/gst"
+	"github.com/go-gst/go-gst/gst/app"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 	"golang.org/x/sync/errgroup"
@@ -32,23 +35,36 @@ func TestWriterNewSample(t *testing.T) {
 	defer goleak.VerifyNone(t, ignore)
 	before := getLeakCount(t)
 	defer checkGStreamerLeaks(t, before)
+	filePath := getFixture("5sec.mp4")
+	fileInfo, err := os.Stat(filePath)
+	require.NoError(t, err)
+	fileSize := fileInfo.Size()
+	t.Logf("Test file size: %d bytes", fileSize)
 	g, ctx := errgroup.WithContext(context.Background())
 	ctx = log.WithDebugValue(ctx, map[string]map[string]int{"func": {"TestWriterNewSample": 9}})
 	for i := 0; i < streamplaceTestRuns; i++ {
 		g.Go(func() error {
-			return writerNewSampleInner(ctx)
+			bs := bytes.Buffer{}
+			err := writerNewSampleInner(ctx, i, &bs)
+			if err != nil {
+				return err
+			}
+			if bs.Len() != int(fileSize) {
+				return fmt.Errorf("expected %d bytes, got %d", fileSize, bs.Len())
+			}
+			return nil
 		})
 	}
-	err := g.Wait()
+	err = g.Wait()
 	require.NoError(t, err)
 }
 
-func writerNewSampleInner(ctx context.Context) error {
+func writerNewSampleInner(ctx context.Context, i int, w io.Writer) error {
 	ctx = log.WithLogValues(ctx, "func", "TestWriterNewSample")
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	pipeline, err := gst.NewPipeline("TestWriterNewSample")
+	pipeline, err := gst.NewPipeline(fmt.Sprintf("TestWriterNewSample-%d", i))
 	if err != nil {
 		return err
 	}
@@ -70,18 +86,24 @@ func writerNewSampleInner(ctx context.Context) error {
 		cancel()
 	}()
 
-	fakeSink, err := gst.NewElementWithProperties("fakesink", map[string]any{
+	appSink, err := gst.NewElementWithProperties("appsink", map[string]any{
+		"name": fmt.Sprintf("TestWriterNewSample-appsink-%d", i),
 		"sync": false,
 	})
 	if err != nil {
 		return err
 	}
-	err = pipeline.Add(fakeSink)
+	err = pipeline.Add(appSink)
 	if err != nil {
 		return err
 	}
 
-	err = fileSrc.Link(fakeSink)
+	sink := app.SinkFromElement(appSink)
+	sink.SetCallbacks(&app.SinkCallbacks{
+		NewSampleFunc: WriterNewSample(ctx, w),
+	})
+
+	err = fileSrc.Link(appSink)
 	if err != nil {
 		return err
 	}

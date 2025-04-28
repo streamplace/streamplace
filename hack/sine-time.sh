0ae646f70cdd82e994dfd6bf6621dccf6579ee51
Eli Mallon <eli@aquareum.tv>
2025-04-28 14:58:43 -0700

    hack/sine-time: add duration

--- hack/sine-time.sh
+++ hack/sine-time.sh
@@ -4,13 +4,15 @@
 
 set -euo pipefail
 
+DURATION="${DURATION:-60}"
+
 ffmpeg -y \
-  -f lavfi -i "aevalsrc=0.125 * sin(2 * PI * (150+(800*mod(t\,1))) * t):d=1:sample_rate=48000,aloop=loop=60:size=48000*60" \
+  -f lavfi -i "aevalsrc=0.125 * sin(2 * PI * (150+(800*mod(t\,1))) * t):d=1:sample_rate=48000,aloop=loop=${DURATION}:size=48000*${DURATION}" \
   -filter_complex "
     color=c=green:s=1280x360:r=60:d=1,format=yuv420p[red1];
     color=c=blue:s=1280x360:r=60:d=1,format=yuv420p[blue];
     [red1][blue]blend=all_expr='A*(1-T)+B*T'[fadeout];
-    [fadeout]loop=loop=60:size=120[colorfade];
+    [fadeout]loop=loop=${DURATION}:size=${DURATION}*2[colorfade];
     [0:a]asplit[audio][audio2];
     [audio2]showwaves=split_channels=1:s=1280x360:rate=25,fps=60[waveform];
     [colorfade][waveform]vstack[video];
@@ -21,11 +23,11 @@ ffmpeg -y \
   -preset ultrafast \
   -g 60 \
   -keyint_min 60 \
-  -profile:v main \
+  -x264-params "keyint=60:scenecut=0:bframes=0" \
   -crf 23 \
   -c:a libopus \
   -b:a 128k \
-  -t 60 \
+  -t "${DURATION}" \
   output_looped.mp4
 
 ffplay output_looped.mp4

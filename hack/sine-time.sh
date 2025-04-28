a7b9d54a46f7dc2762db24ce306c4669b8fc6c23
Eli Mallon <eli@aquareum.tv>
2025-04-28 16:03:31 -0700

    sine-time

--- hack/sine-time.sh
+++ hack/sine-time.sh
@@ -5,16 +5,19 @@
 set -euo pipefail
 
 DURATION="${DURATION:-60}"
+WIDTH="${WIDTH:-1280}"
+HEIGHT="${HEIGHT:-720}"
+HALF_HEIGHT=$((HEIGHT / 2))
 
 ffmpeg -y \
   -f lavfi -i "aevalsrc=0.125 * sin(2 * PI * (150+(800*mod(t\,1))) * t):d=1:sample_rate=48000,aloop=loop=${DURATION}:size=48000*${DURATION}" \
   -filter_complex "
-    color=c=green:s=1280x360:r=60:d=1,format=yuv420p[red1];
-    color=c=blue:s=1280x360:r=60:d=1,format=yuv420p[blue];
-    [red1][blue]blend=all_expr='A*(1-T)+B*T'[fadeout];
+    color=c=green:s=${WIDTH}x${HALF_HEIGHT}:r=60:d=1,format=yuv420p[green];
+    color=c=blue:s=${WIDTH}x${HALF_HEIGHT}:r=60:d=1,format=yuv420p[blue];
+    [green][blue]blend=all_expr='if(gte((N/60),X/W),A,B)'[fadeout];
     [fadeout]loop=loop=${DURATION}:size=${DURATION}*2[colorfade];
     [0:a]asplit[audio][audio2];
-    [audio2]showwaves=split_channels=1:s=1280x360:rate=25,fps=60[waveform];
+    [audio2]showwaves=split_channels=1:s=${WIDTH}x${HALF_HEIGHT}:rate=25,fps=60[waveform];
     [colorfade][waveform]vstack[video];
   " \
   -map "[video]" \
@@ -28,6 +31,7 @@ ffmpeg -y \
   -c:a libopus \
   -b:a 128k \
   -t "${DURATION}" \
-  output_looped.mp4
+  output_looped.mp4 \
+  -report
 
 ffplay output_looped.mp4

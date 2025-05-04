182e0fb3bc6319636ee65ccce7b6d2c5074a918f
Eli Mallon <eli@aquareum.tv>
2025-05-04 13:09:21 -0700

    chore: remove oauth changes

--- js/app/components/login/login.tsx
+++ js/app/components/login/login.tsx
@@ -1,4 +1,3 @@
-import { AtpBaseClient } from "lexicons";
 import NameColorPicker from "components/name-color-picker/name-color-picker";
 import {
   login,
@@ -19,7 +18,6 @@ export default function Login() {
   const pds = useAppSelector(selectPDS);
   const loginState = useAppSelector(selectLogin);
   const [open, setOpen] = useState(false);
-  const [handle, setHandle] = useState("");
   const onOpenChange = (open: boolean) => {
     setOpen(open);
     Keyboard.dismiss();
@@ -53,40 +51,24 @@ export default function Login() {
   }
 
   return (
-    <View
-      f={1}
-      jc="center"
-      ai="center"
-      backgroundColor="$gray1"
-      padding="$4"
-      width="100%"
-      maxWidth={800}
-      marginHorizontal="auto"
-    >
+    <View f={1} jc="center" ai="center" backgroundColor="$gray1" padding="$4">
       <ChangePDS open={open} onOpenChange={onOpenChange} />
       {/* <Text>{error}</Text> */}
-      <H3>Handle</H3>
-      <Input
-        width="100%"
-        placeholder="example.bsky.social"
-        value={handle}
-        onChangeText={(text) => setHandle(text)}
-      />
       <Button
         width="100%"
         onPress={async () => {
-          const agent = new AtpBaseClient(`http://127.0.0.1:38080`);
-          const res = await agent.place.stream.account.login({
-            handleOrDID: handle,
-          });
-          console.log(res);
-          // await dispatch(login(`https://${pds.url}`));
+          await dispatch(login(`https://${pds.url}`));
         }}
         margin="$4"
         backgroundColor="$accentColor"
         disabled={loginState.loading}
       >
-        <Text>{loginState.loading ? <Spinner /> : `Log in with ATProto`}</Text>
+        <Text>
+          {loginState.loading ? <Spinner /> : `Log in with ${pds.url}`}
+        </Text>
+      </Button>
+      <Button width="100%" onPress={() => onOpenChange(true)} margin="$4">
+        Change PDS
       </Button>
     </View>
   );
--- pkg/spxrpc/stubs.go
+++ pkg/spxrpc/stubs.go
@@ -6,7 +6,6 @@ import (
 	appbskytypes "github.com/bluesky-social/indigo/api/bsky"
 	"github.com/labstack/echo/v4"
 	"go.opentelemetry.io/otel"
-	placestreamtypes "stream.place/streamplace/pkg/streamplace"
 )
 
 func (s *Server) RegisterHandlersAppBsky(e *echo.Echo) error {
@@ -49,26 +48,7 @@ func (s *Server) RegisterHandlersComAtproto(e *echo.Echo) error {
 }
 
 func (s *Server) RegisterHandlersPlaceStream(e *echo.Echo) error {
-	e.POST("/xrpc/place.stream.account.login", s.HandlePlaceStreamAccountLogin)
 	return nil
-}
-
-func (s *Server) HandlePlaceStreamAccountLogin(c echo.Context) error {
-	ctx, span := otel.Tracer("server").Start(c.Request().Context(), "HandlePlaceStreamAccountLogin")
-	defer span.End()
-
-	var body placestreamtypes.AccountLogin_Input
-	if err := c.Bind(&body); err != nil {
-		return err
-	}
-	var out *placestreamtypes.AccountDefs_LoginResponse
-	var handleErr error
-	// func (s *Server) handlePlaceStreamAccountLogin(ctx context.Context,body *placestreamtypes.AccountLogin_Input) (*placestreamtypes.AccountDefs_LoginResponse, error)
-	out, handleErr = s.handlePlaceStreamAccountLogin(ctx, &body)
-	if handleErr != nil {
-		return handleErr
-	}
-	return c.JSON(200, out)
 }
 
 func (s *Server) RegisterHandlersToolsOzone(e *echo.Echo) error {

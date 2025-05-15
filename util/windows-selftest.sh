685fc1408d18a34728f6699bad1a9cf69efacb2d
Eli Mallon <eli@aquareum.tv>
2025-05-15 15:56:56 -0700

    ci: it's like app data but local-er

--- util/windows-selftest.sh
+++ util/windows-selftest.sh
@@ -6,6 +6,6 @@ set -x
 curl -L -o streamplace-desktop.exe "$1"
 ./streamplace-desktop.exe
 powershell -Command "Stop-Process -Name Streamplace"
-cd /c/Users/admin/AppData/Local/streamplace_desktop
+cd "$LOCALAPPDATA/streamplace_desktop"
 cd app-*
 ./Streamplace.exe -- --self-test

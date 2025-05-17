21546c8cfd3f288f7bf5c1515838521a7585a170
Eli Mallon <eli@aquareum.tv>
2025-05-16 17:09:41 -0700

    app: make use of place.stream.live.getSegments

--- js/app/hooks/useLiveUser.tsx
+++ js/app/hooks/useLiveUser.tsx
@@ -1,16 +1,23 @@
-import { selectUserProfile } from "features/bluesky/blueskySlice";
-import { selectRecentSegments } from "features/streamplace/streamplaceSlice";
+import { selectMySegments } from "features/streamplace/streamplaceSlice";
+import { isRecord } from "lexicons/types/place/stream/segment";
 import { useAppSelector } from "store/hooks";
+import { PlaceStreamSegment } from "lexicons";
 
 // composite selector that tells us when the current user is live
 export const useLiveUser = (): boolean => {
-  const profile = useAppSelector(selectUserProfile);
-  const { segments } = useAppSelector(selectRecentSegments);
-  if (!profile) {
+  const mySegments = useAppSelector(selectMySegments);
+  if (mySegments.length === 0) {
     return false;
   }
-  const isLive = segments.some(
-    (segment) => segment.repo && segment.repo.did === profile.did,
-  );
-  return isLive;
+  if (!isRecord(mySegments[0].record)) {
+    return false;
+  }
+  const record = mySegments[0].record as PlaceStreamSegment.Record;
+  console.log(new Date(record.startTime).getTime() - Date.now());
+  if (Date.now() - new Date(record.startTime).getTime() < 1000 * 10) {
+    console.log("user is live", record.startTime);
+    return true;
+  }
+  console.log("user is not live", record.startTime);
+  return false;
 };

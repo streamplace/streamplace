66582b25db69f98cd4d36255bc5e3a274edc2eab
Eli Mallon <eli@iame.li>
2025-09-21 15:13:20 -0700

    determinism: add lots of split and combine testing

--- pkg/media/deterministic_muxing_test.go
+++ pkg/media/deterministic_muxing_test.go
@@ -21,24 +21,53 @@ func TestDeterministicMuxing(t *testing.T) {
 		tempDir, err := os.MkdirTemp("", "deterministic_muxing_test")
 		require.NoError(t, err)
 		defer os.RemoveAll(tempDir)
-
-		sixtySecFile := remote.RemoteFixture("7629bc0e2b3964743bfee06cc59dceabc5b4637b19de8e3fea0889cb421782d8/perfect.mp4")
-		segDirs := []string{}
+		startFile := remote.RemoteFixture("7629bc0e2b3964743bfee06cc59dceabc5b4637b19de8e3fea0889cb421782d8/perfect.mp4")
 		for i := 0; i < muxTestCount; i++ {
-			segDir, err := os.MkdirTemp(tempDir, "segs")
-			segDirs = append(segDirs, segDir)
-			require.NoError(t, err)
-			err = SegmentFile(context.Background(), sixtySecFile, segDir)
-			require.NoError(t, err)
-		}
-		firstReport, err := makeSegDirReport(t, segDirs[0])
-		require.NoError(t, err)
-		for _, segDir := range segDirs[1:] {
-			report, err := makeSegDirReport(t, segDir)
-			require.NoError(t, err)
-			require.True(t, report.Equals(firstReport))
+			startFile = splitAndCombineTest(t, tempDir, startFile)
 		}
 	})
+}
+
+func splitAndCombineTest(t *testing.T, tempDir string, inputPath string) string {
+	var err error
+	tempDir, err = os.MkdirTemp(tempDir, "splitAndCombineTest")
+	require.NoError(t, err)
+
+	segDirs := []string{}
+	for i := 0; i < muxTestCount; i++ {
+		segDir, err := os.MkdirTemp(tempDir, "segs")
+		segDirs = append(segDirs, segDir)
+		require.NoError(t, err)
+		err = SegmentFile(context.Background(), inputPath, segDir)
+		require.NoError(t, err)
+	}
+	firstReport, err := makeSegDirReport(t, segDirs[0])
+	require.NoError(t, err)
+	for _, segDir := range segDirs[1:] {
+		report, err := makeSegDirReport(t, segDir)
+		require.NoError(t, err)
+		require.True(t, report.Equals(firstReport))
+	}
+
+	combinedHashes := []string{}
+	combinedFiles := []string{}
+	for i := 0; i < muxTestCount; i++ {
+		outFilePath := filepath.Join(tempDir, fmt.Sprintf("combined_%d.mp4", i))
+		combinedFiles = append(combinedFiles, outFilePath)
+		outFile, err := os.Create(outFilePath)
+		require.NoError(t, err)
+		defer outFile.Close()
+		err = Clip(context.Background(), firstReport.Segs, outFile)
+		require.NoError(t, err)
+		hash, err := hashFile(outFilePath)
+		require.NoError(t, err)
+		combinedHashes = append(combinedHashes, hash)
+	}
+
+	for _, hash := range combinedHashes {
+		require.Equal(t, hash, combinedHashes[0])
+	}
+	return combinedFiles[0]
 }
 
 type SegDirReport struct {
@@ -53,18 +82,17 @@ func makeSegDirReport(t *testing.T, segDir string) (*SegDirReport, error) {
 	require.NoError(t, err)
 	for _, segEntry := range segEntries {
 		if segEntry.Type().IsRegular() {
-			segs = append(segs, segEntry.Name())
+			segPath := filepath.Join(segDir, segEntry.Name())
+			segs = append(segs, segPath)
 		}
 	}
 	sort.Strings(segs)
 	hashes := make([]string, len(segs))
-	for i, seg := range segs {
-		segPath := filepath.Join(segDir, seg)
-		segData, err := os.ReadFile(segPath)
+	for i, segPath := range segs {
+		hash, err := hashFile(segPath)
 		if err != nil {
 			return nil, err
 		}
-		hash := sha256.Sum256(segData)
 		hashes[i] = fmt.Sprintf("%x", hash)
 	}
 
@@ -83,4 +111,13 @@ func (s *SegDirReport) Equals(other *SegDirReport) bool {
 		return false
 	}
 	return reflect.DeepEqual(s.Hashes, other.Hashes)
+}
+
+func hashFile(path string) (string, error) {
+	bs, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	hash := sha256.Sum256(bs)
+	return fmt.Sprintf("%x", hash), nil
 }

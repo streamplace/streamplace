b08158d4b8affedb5b6e06e46e466d039da71987
Eli Mallon <eli@iame.li>
2026-03-12 19:56:56 -0700

    app: add migration path for old dpop keys

--- js/app/src/polyfills.native.tsx
+++ js/app/src/polyfills.native.tsx
@@ -1,5 +1,10 @@
 import "expo-crypto";
-import Storage from "expo-sqlite/kv-store";
+import sqliteStorage from "expo-sqlite/kv-store";
+import { MMKV } from "react-native-mmkv";
+
+const ATPROTO_OAUTH_MMKV_NAME = "expo-atproto-oauth-client.session";
+const OLD_SQLITE_STORE_PREFIX = "@@atproto/oauth-client-react-native:session:";
+
 global.AbortSignal.timeout = function (ms) {
   const controller = new AbortController();
 
@@ -14,6 +19,38 @@ global.AbortSignal.prototype.throwIfAborted = function () {
   if (!aborted) return;
   throw reason;
 };
-Storage.getAllKeysSync().forEach((key) => {
-  console.log("key", key);
-});
+const allKeys = sqliteStorage.getAllKeysSync();
+
+function migrateOAuthStorage() {
+  const mmkvStorage = new MMKV({
+    id: ATPROTO_OAUTH_MMKV_NAME,
+  });
+
+  for (const key of allKeys) {
+    if (!key.startsWith(OLD_SQLITE_STORE_PREFIX)) {
+      continue;
+    }
+    const did = key.split(OLD_SQLITE_STORE_PREFIX)[1];
+    const oldValue = sqliteStorage.getItemSync(key);
+    if (!oldValue) {
+      console.warn(`[oauth-migration] no value found for ${key}, skipping`);
+      continue;
+    }
+    const newValue = mmkvStorage.getString(did);
+    if (newValue) {
+      console.warn(
+        `[oauth-migration] looks like we're already logged in on mmkv, no need to migrate ${did}`,
+      );
+      continue;
+    }
+    console.log(`[oauth-migration] migrating ${did}`);
+    mmkvStorage.set(did, oldValue);
+    sqliteStorage.removeItem(key);
+  }
+}
+
+try {
+  migrateOAuthStorage();
+} catch (error) {
+  console.error(`[oauth-migration] error migrating storage`, error);
+}

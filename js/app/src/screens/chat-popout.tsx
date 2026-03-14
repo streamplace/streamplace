6ca0f861f95b4807a858e5330341943aa896941e
Natalie B. <22222885+espeon@users.noreply.github.com>
2026-03-14 15:30:28 -0500

    add proper log in prompt, fix up queries

--- js/app/src/screens/chat-popout.tsx
+++ js/app/src/screens/chat-popout.tsx
@@ -3,12 +3,16 @@ import {
   ChatBox,
   LivestreamProvider,
   PlayerProvider,
+  Text,
+  tokens,
   usePlayerStore,
   zero,
 } from "@streamplace/components";
 import { EmojiPicker } from "components/emoji-picker/emoji-picker";
+import { ArrowRight } from "lucide-react-native";
 import { useEffect } from "react";
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
+import { useStore } from "store";
 import { useUserProfile } from "store/hooks";
 import { useEmojiData } from "utils/emoji";
 
@@ -43,6 +47,16 @@ export function PopoutChatInner({ params }: { params: ChatPopoutParams }) {
   const setSrc = usePlayerStore((x) => x.setSrc);
   const profile = useUserProfile();
   const emojiData = useEmojiData();
+  const hideSidebar = useStore((x) => x.setSidebarHidden);
+  const showSidebar = useStore((x) => x.setSidebarUnhidden);
+  const openLoginModal = useStore((x) => x.openLoginModal);
+
+  useEffect(() => {
+    hideSidebar();
+    return () => {
+      showSidebar();
+    };
+  }, [showSidebar]);
 
   const reverseChat = params.reverse === "true";
   const hideAfter = params.hideAfter
@@ -62,28 +76,40 @@ export function PopoutChatInner({ params }: { params: ChatPopoutParams }) {
           { position: "absolute", width: "100%", minHeight: "100%", bottom: 0 },
         ]}
       >
-        <Chat
-          {...(reverseChat || hideAfter
-            ? ({
-                ...(reverseChat ? { reverse: true } : {}),
-                ...(hideAfter ? { hideAfter } : {}),
-              } as any)
-            : {})}
-        />
-        {profile && !hideChatBox && (
-          <ChatBox
-            emojiData={emojiData}
-            isPopout={true}
-            emojiPicker={(isOpen, onClose, onSelect) => (
-              <EmojiPicker
-                isOpen={isOpen}
-                onClose={onClose}
-                onSelect={onSelect}
-                customEmoji={[]}
-              />
-            )}
-          />
-        )}
+        <Chat reverse={reverseChat} hideAfter={hideAfter} />
+        {!hideChatBox &&
+          (profile ? (
+            <ChatBox
+              emojiData={emojiData}
+              isPopout={true}
+              emojiPicker={(isOpen, onClose, onSelect) => (
+                <EmojiPicker
+                  isOpen={isOpen}
+                  onClose={onClose}
+                  onSelect={onSelect}
+                  customEmoji={[]}
+                />
+              )}
+            />
+          ) : (
+            <Pressable
+              onPress={() => openLoginModal({ name: "ChatPopout" })}
+              style={[
+                zero.layout.flex.row,
+                zero.layout.flex.center,
+                zero.gap.all[4],
+                zero.r.xl,
+                {
+                  padding: 18,
+                  backgroundColor: "rgba(255, 255, 255, 0.1)",
+                  maxWidth: tokens.breakpoints.sm,
+                },
+              ]}
+            >
+              <Text>Log in or sign up to chat</Text>
+              <ArrowRight />
+            </Pressable>
+          ))}
       </View>
     </View>
   );

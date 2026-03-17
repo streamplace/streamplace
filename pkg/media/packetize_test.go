46b8159f7e112ed6e2f57c27720b0d7104f81c8c
Eli Mallon <eli@iame.li>
2026-03-17 15:17:08 -0700

    media: fix thumbnailing fmp4

--- pkg/media/packetize_test.go
+++ pkg/media/packetize_test.go
@@ -19,7 +19,7 @@ func TestPacketize(t *testing.T) {
 		g, _ := errgroup.WithContext(context.Background())
 		for range streamplaceTestCount {
 			g.Go(func() error {
-				innerTestPacketize(t, getFixture("sample-segment.mp4"))
+				innerTestPacketize(t, getFixture("sample-segment.mp4"), 49, 40, time.Duration(800*time.Millisecond))
 				return nil
 			})
 		}
@@ -31,11 +31,11 @@ func TestPacketize(t *testing.T) {
 func TestPacketizeMuxl(t *testing.T) {
 	withNoGSTLeaks(t, func() {
 		filename := remote.RemoteFixture("c6b57a53fc5a2234dbdd388922f0e293d8063d2b30620321e974b7c85640f228/2026-03-17T19-02-08-607Z-muxl_segment_input.fmp4")
-		innerTestPacketize(t, filename)
+		innerTestPacketize(t, filename, 60, 50, time.Duration(1000*time.Millisecond))
 	})
 }
 
-func innerTestPacketize(t *testing.T, filename string) {
+func innerTestPacketize(t *testing.T, filename string, expectedVideo int, expectedAudio int, expectedDuration time.Duration) {
 	inputFile, err := os.Open(filename)
 	require.NoError(t, err)
 	defer inputFile.Close()
@@ -51,9 +51,9 @@ func innerTestPacketize(t *testing.T, filename string) {
 	packet, err := Packetize(context.Background(), testSeg)
 	require.NoError(t, err)
 	require.NotNil(t, packet)
-	require.Equal(t, 49, len(packet.Video))
-	require.Equal(t, 40, len(packet.Audio))
-	require.Equal(t, time.Duration(800*time.Millisecond), packet.Duration)
+	require.Equal(t, expectedVideo, len(packet.Video))
+	require.Equal(t, expectedAudio, len(packet.Audio))
+	require.Equal(t, expectedDuration, packet.Duration)
 }
 
 func TestPacketizeInvalid(t *testing.T) {

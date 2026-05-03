3211524185df3da8aba70d8ca621e18f2850af3e
Eli Mallon <eli@iame.li>
2026-05-03 15:15:21 -0700

    tracing: allow basic auth

--- pkg/cmd/tracing.go
+++ pkg/cmd/tracing.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"context"
+	"encoding/base64"
+	"net/url"
 	"time"
 
 	"go.opentelemetry.io/otel"
@@ -25,7 +27,23 @@ func startTelemetry(ctx context.Context, endpoint string) error {
 }
 
 func newTracerProvider(ctx context.Context, endpoint string) (*trace.TracerProvider, error) {
-	exp, err := otlptracehttp.New(ctx, otlptracehttp.WithEndpointURL(endpoint))
+	parsedURL, err := url.Parse(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := []otlptracehttp.Option{otlptracehttp.WithEndpointURL(endpoint)}
+
+	if parsedURL.User != nil {
+		username := parsedURL.User.Username()
+		password, _ := parsedURL.User.Password()
+		encoded := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
+		opts = append(opts, otlptracehttp.WithHeaders(map[string]string{
+			"Authorization": "Basic " + encoded,
+		}))
+	}
+
+	exp, err := otlptracehttp.New(ctx, opts...)
 	if err != nil {
 		return nil, err
 	}
